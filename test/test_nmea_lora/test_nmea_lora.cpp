0c4799033552242180a30d720ee7fa126f3b19ab
kirazero17 <84077194+kirazero17@users.noreply.github.com>
2026-04-25 01:57:49 +0700

    thêm test nmea

---
+++ test/test_nmea_lora/test_nmea_lora.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include <unity.h>
+
+void setUp(void) {
+  ArduinoFakeReset();
+
+  fakeit::When(OverloadedMethod(ArduinoFake(Serial), print, size_t(const char[]))).AlwaysReturn(1);
+  fakeit::When(OverloadedMethod(ArduinoFake(Serial), print, size_t(int, int))).AlwaysReturn(1);
+  fakeit::When(OverloadedMethod(ArduinoFake(Serial), println, size_t(const char[]))).AlwaysReturn(1);
+  fakeit::When(OverloadedMethod(ArduinoFake(Serial), println, size_t(const String&))).AlwaysReturn(1);
+
+  fakeit::When(OverloadedMethod(ArduinoFake(Function), random, long(long))).Return(1234);
+  fakeit::When(Method(ArduinoFake(Function), delay)).AlwaysReturn();
+}
+
+void tearDown(void) {}
+
+#ifndef NATIVE_BUILD
+void setup() {}
+
+void loop() {}
+#endif
+
+int main() {
+  UNITY_BEGIN();
+  
+  return UNITY_END();
+}
